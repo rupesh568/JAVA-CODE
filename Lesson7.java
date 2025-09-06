41f85b24fdc2e3efcd132c16a4ba0f20ebedda6f
Rupesh568 <rupeshjaiswal999939@gmail.com>
2025-09-06 23:14:09 +0530

    Type casting in Java

---
+++ Lesson7.java
@@ -0,0 +1,14 @@
+//Implict type casting;
+
+public class Lesson7 {
+    public static void main(String[] args){
+        int a=4;
+        float b=5.5f;
+        float sum=a+b;
+        System.out.println("The sum is:"+sum);
+    }
+}
+
+//Explict type casting;
+
+
---
+++ Lesson8.java
@@ -0,0 +1,10 @@
+//Explict type casting
+
+public class Lesson8{
+    public static void main(String[] args){
+        long c=4344555L;
+        int myInt=(int) c;
+        System.out.println("The long value is:"+c);
+        System.out.println("The integer value is:"+myInt);
+    }
+}
