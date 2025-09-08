6754939e3693cda61f455f3104f2de77649657cc
Rupesh568 <rupeshjaiswal999939@gmail.com>
2025-09-08 23:44:01 +0530

    Loop in java

---
+++ Lesson13.java
@@ -0,0 +1,31 @@
+//Loops :Loops is used to repeat instructions;
+//for loop;
+public class Lesson13 {
+    public static void main(String[] args){
+        for(int i=0;i<=5;i++){
+            System.out.println("hello world");
+        }
+    }
+}
+
+//While loop;
+class First{
+    public static void main(String[] args){
+        int i=0;
+        while(i<=10){
+            System.out.println("i="+i);
+            i++;
+        }
+    }
+}
+
+//do while loop;
+class Hello{
+    public static void main(String[] args){
+        int i=1;
+        do{
+            System.out.println("Hello world");
+            i++;
+        }while(i<=4);
+    }
+}
